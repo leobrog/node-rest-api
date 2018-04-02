fa94afe6ab28eac3eef823e89e065322ab0a63d9
leobrog <30665342+leobrog@users.noreply.github.com>
2018-04-02 15:37:57 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
