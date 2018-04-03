8f7f338f177eaebe089a8e5fbec2131caea58103
leobrog <30665342+leobrog@users.noreply.github.com>
2018-04-03 16:39:01 -0300

    Adding comments to server.js and taskRoutes.js

--- api/routes/taskRoutes.js
+++ api/routes/taskRoutes.js
@@ -4,15 +4,33 @@ module.exports = (app) => {
 
     // task routes
 
+    /*
+    * routes for /api/tasks
+    * GET : gets all tasks in tasks collection
+    * POST : creates a new task => Req.body requires name attribute
+    *
+    */
     app.route('/api/tasks')
         .get(tasks.listAllTasks)
         .post(tasks.createTask);
 
+    /*
+    * routes for /api/tasks/:taskId
+    * GET : gets single task by its ID
+    * PUT : updates a single task by its ID
+    * DELETE : deletes a single task by its ID
+    */    
     app.route('/api/tasks/:taskId')
         .get(tasks.readTask)
         .put(tasks.updateTask)
         .delete(tasks.deleteTask);
 
+    /* 
+    * routes for /api/tasks/status/:status
+    * GET : gets tasks by their status (pending, ongoing, completed)
+    *
+    *
+    */
     app.route('/api/tasks/status/:status')
         .get(tasks.listTasksByStatus);
 };
--- server.js
+++ server.js
@@ -1,27 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const hbs = require('hbs');
+//const hbs = require('hbs');
 
+// Use either the host environment port or hardcoded 3000 port.
 const port = process.env.PORT || 3000;
 
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine', 'hbs');
+// Setting up views engine (currently not being used)
+//hbs.registerPartials(__dirname + '/views/partials');
+//app.set('view engine', 'hbs');
 
-// Server request Log middleware.
+// Server's request Logger middleware. Log available at server.log.
 app.use( require('./utilities/server-logger/serverLogger').requestLogger);
 
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear();
-});
+// hbs.registerHelper('getCurrentYear', () => {
+//     return new Date().getFullYear();
+// });
 
+// Importing Tasks mongoose model.
 var Task = require('./api/models/taskModel');
 
-var db = 'mongodb://localhost:27017'; // mLab URI: mongodb://<dbuser>:<dbpassword>@ds059207.mlab.com:59207/node-rest-api-tasks
-//var db = 'mongodb://leob:123456@ds059207.mlab.com:59207/node-rest-api-tasks'
+// Database connection URI's. Use localhost when connection to remote DBserver is not available.
 
+// Localhost connection URI
+//var db = 'mongodb://localhost:27017'; 
+
+// mLab URI: mongodb://<dbuser>:<dbpassword>@ds059207.mlab.com:59207/node-rest-api-tasks
+var db = 'mongodb://leob:123456@ds059207.mlab.com:59207/node-rest-api-tasks'
+
+//MongoDB connection promise
 mongoose.Promise = global.Promise;
 mongoose.connect(db, (err) => {
     if (err) {
@@ -34,7 +43,7 @@ mongoose.connect(db, (err) => {
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 
-// Tasks API Routes
+// Registering Tasks API Routes. See @ api/routes/taskRoutes
 var taskRoutes = require('./api/routes/taskRoutes');
 taskRoutes(app);
 
